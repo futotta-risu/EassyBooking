c577140f79159f842be2babd8370fbb32eb6138f
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2020-12-29 17:18:12 +0100

    Assenblers

---
+++ src/org/SlavaLenin/EassyBooking/app/dto/FligthAssembler.java
@@ -0,0 +1,25 @@
+package org.SlavaLenin.EassyBooking.app.dto;
+
+import java.util.List;
+
+import org.SlavaLenin.EassyBooking.app.data.Flight;
+
+
+
+
+public class FligthAssembler {
+
+	public FligthAssembler() {
+		super();
+		
+	}
+
+	public FligthDTO assenble (Flight fligth) {
+		return null;
+		
+	}
+	
+	public void assenble (List<Flight>fligths) {
+		
+	}
+}
---
+++ src/org/SlavaLenin/EassyBooking/app/dto/UserAssembler.java
@@ -0,0 +1,24 @@
+package org.SlavaLenin.EassyBooking.app.dto;
+
+import org.SlavaLenin.EassyBooking.app.data.User;
+
+public class UserAssembler {
+
+	
+	public UserAssembler() {
+		super();
+		
+	}
+
+	public UserDTO assenble (User user) {
+			
+
+		
+		
+		return null;
+		
+	
+	}
+
+	
+}
