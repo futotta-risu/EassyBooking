2923a5725ebc64341d6083565f83f069db132bac
gorkazarate <52033110+gorkazarate@users.noreply.github.com>
2021-01-20 13:11:31 +0100

    Update AirFranceGateway.java

--- src/org/SlavaLenin/EassyBooking/app/gateway/airline/AirFranceGateway.java
+++ src/org/SlavaLenin/EassyBooking/app/gateway/airline/AirFranceGateway.java
@@ -53,22 +53,21 @@ public class AirFranceGateway implements AirlineGateway {
 	@Override
 	public List<Flight> buscar(String id) {
 		Logger.getLogger(ServerManagerFrame.class.getName()).info("AirFranceGateway: buscar con " + id);
-		System.out.println("AirFrance se esta ejecuntando ");
 		List<Flight> flights = new ArrayList<Flight>();
 		try{
-			System.out.println("AirFrance se esta ejecuntando 2");
+			
 			Socket tcpSocket = new Socket(IP, PORT);
 		
 			//Streams to send and receive information are created from the Socket
 			ObjectInputStream in = new ObjectInputStream(tcpSocket.getInputStream());
 			DataOutputStream out = new DataOutputStream(tcpSocket.getOutputStream());
-			System.out.println("AirFrance se esta ejecuntando 3");
+			System.out.println("AirFrance has inicied conexion");
 			out.writeUTF("BUSCAR "+ id);
 			
 			System.out.println("- EchoClient: Sent data to '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> 'BUSCAR " + id + "'");	
 			@SuppressWarnings("unchecked")
 			List<SocketAirlineFlightDTO> dataDTO = (List<SocketAirlineFlightDTO>) in.readObject();
-			System.out.println("AirFrance ha recivido " + dataDTO.size());
+			System.out.println("AirFrance has recived:  " + dataDTO.size());
 			for(SocketAirlineFlightDTO flightDTO : dataDTO) {
 				Flight f = new Flight();
 				f.setFlightNumber(flightDTO.getFligthNumber());
@@ -80,7 +79,7 @@ public class AirFranceGateway implements AirlineGateway {
 				f.setAirportArrival(flightDTO.getAirportArrival());
 				flights.add(f);
 			}
-			System.out.println("SASJDSADOASWJDJOASo vuelos SOCKET" + dataDTO.size());
+			System.out.println("Has send flights: "+flights);
 			
 			return flights;
 		}catch (Exception e) {
